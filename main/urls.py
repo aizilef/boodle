e49fea0ecd7d49a79f2e29c887af4601ce6cdd42
Felizia Tiburcio <lizzytiburcio3@gmail.com>
2022-03-11 21:11:43 +0800

    Rough layout for auction page

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import homepage
+from .views import *
 
 
 urlpatterns = [
     path('', homepage, name='index'),
+    path('auction', auction, name='auction'),
+    path('test',test,name='test'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,4 +5,11 @@ from django.http import HttpResponse
 def homepage(request):
     return render(request, "boodlesite/templates/index.html")    
 
+def auction(request):
+    return render(request, "boodlesite/templates/auction.html")    
+
+def test(request):
+    return HttpResponse("Hellotest")
+
+
 

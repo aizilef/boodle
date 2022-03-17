d351f1326345195978785863d876168500b8d277
Felizia Tiburcio <lizzytiburcio3@gmail.com>
2022-03-17 08:41:42 +0800

    Create AuctionBid and Item models

--- main/models.py
+++ main/models.py
@@ -7,15 +7,6 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-class Item(models.Model):
-    itemid = models.AutoField(primary_key=True)
-    itemname = models.CharField(max_length=255)
-    itemspecs = models.CharField(max_length=700)
-    floorprice = models.FloatField()
-    sellprice = models.FloatField()
-    class Meta:
-        managed = False
-        db_table = 'item'
 
 class Auction(models.Model):
     auctionid = models.AutoField(primary_key=True)
@@ -23,7 +14,7 @@ class Auction(models.Model):
     info = models.CharField(max_length=255)
     auctionstart = models.DateTimeField()
     auctionend = models.DateTimeField()
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    itemid = models.ForeignKey('Item', models.DO_NOTHING, db_column='itemid', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -32,9 +23,132 @@ class Auction(models.Model):
 
 class AuctionBid(models.Model):
     bidno = models.AutoField(primary_key=True)
-    amount = models.FloatField()
-    bidtime = models.DateTimeField()
-    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=15, decimal_places=4)
+    bidtime = models.DateTimeField(blank=True, null=True)
+    auctionid = models.ForeignKey(Auction, models.DO_NOTHING, db_column='auctionid')
+
     class Meta:
         managed = False
-        db_table = 'auction_bid'
+        db_table = 'auctionbid'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Item(models.Model):
+    itemid = models.AutoField(primary_key=True)
+    itemname = models.CharField(max_length=255)
+    itemspecs = models.CharField(max_length=700)
+    floorprice = models.DecimalField(max_digits=15, decimal_places=4)
+    sellprice = models.DecimalField(max_digits=15, decimal_places=4, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'item'
